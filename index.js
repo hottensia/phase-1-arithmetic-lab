1d803161fa5de636f4cab2ac5d14745cca9cdbf4
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-03-27 11:22:37 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+    let multiply
+    let num1=2
+    let num2=31
+     multiply= num1*num2
+    
+      let random =Math.floor(Math.random() * 10) + 1;
+
+      
+      let num3 =5
+      let num4 =29
+      let mod = num4 % num3;
+
+     let max= Math.max(1, 2, 3, 15, 20);
+
+
+     
+
